5f47993f3c1ff0514c894c88f9dc6955afc66106
Elias Haroun <e_harou@grace.encs.concordia.ca>
2016-10-16 12:35:46 -0400

    Added Search interface. Fixed tokenizer.

--- personal/elias/Search.py
+++ personal/elias/Search.py
@@ -1,17 +1,115 @@
-import pickle
-import thread
-from pandas.io.wb import search
+import json
+import re
+
+from collections import OrderedDict
+from matplotlib.pyplot import flag
+from nltk.corpus import stopwords
+from nltk import  word_tokenize
+from string import translate
+from CodeWarrior.Standard_Suite import document
+
+def style(text, type):
+	ansi = {'underline': '\033[4m', 'bold': '\033[1m', 'end':'\033[0m\x1b[0m', \
+		'blue':'\033[94m', 'green':'\033[92m' , 'yellow':'\033[93m', \
+		'red':'\033[91m', 'hlg':'\x1b[6;30;42m', 'hly':'\x1b[6;30;43m'}
+	return ansi[type]+'{}{[end]}'.format(text, ansi)
+
+# if no logical operator is specified then resort to AND
+def getQueryParams(query):
+	logicalOperator = ''
+	query = re.sub(r'\'s|\'re|\'d|\'ll', '', query)
+	# Remove non-ASCII values
+	query = re.sub(r'[^\x00-\x7F]+', ' ', query)
+	# Remove unneeded spaces
+	query = re.sub(r'\s{2,5}', ' ', query)
+	# Remove numbers
+	query = re.sub(r'\d+', '', query)
+	# All lower case
+	query = query.lower()
+	# Tokenization of words and removal of punctuation
+	query = set(nltk.word_tokenize(query.translate(None, string.punctuation)))
+	# get logical operators AND/OR
+	if 'or' in query:
+		logicalOperator = 'OR'
+	elif len(query) == 1 and 'and' not in query: # Single term query
+		logicalOperator = None
+	else:										# Either multiple word query with no OR, so AND // And is in the query
+		logicalOperator = 'AND'
+
+	# Removing Stopwords
+	query = query - set(stopwords.words('english'))
+	
+	return {logicalOperator: query}
+
+def titleIndoc(document, query):
+	title = ''
+	# Get the title of the article (document) 
+	titleMatch = re.search(r'^(.*?)#####', document)
+	if titleMatch:
+		title = titleMatch.group(1)
+		queryTitleMatch = re.search(query, title, re.I)
+		if queryTitleMatch:
+			title = title.replace(query.upper(), style(query.upper(), 'hlg'))
+	return (title, queryTitleMatch)
+
+def bodyInDoc(document, query):
+	displayedSnipet = ''
+	# Get a sample phrase from the body
+	bodyMatch = re.search(r'#####(.*?)$', document)
+	if bodyMatch:
+		body = bodyMatch.group(1)
+		displayedSnipetMatch = re.search(r'(\.\s|\W)(.{0,80}' + query + '(?![a-zA-Z0-9_+-]).{0,80})\s', body, re.I)
+		if displayedSnipetMatch:
+			displayedSnipet = displayedSnipetMatch.group(2)+'...'
+			displayedSnipet = displayedSnipet.replace(query, style(query, 'hly')) # "term"
+			displayedSnipet = displayedSnipet.replace(query[0].upper()+query[1:], style(query[0].upper()+query[1:], 'hly')) #"Term"
+			displayedSnipet = displayedSnipet.replace(query.upper(), style(query.upper(), 'hly')) # "TERM"
+	return (displayedSnipet, displayedSnipetMatch)
 
 def search():
-	query = raw_input("Search for: ")
-	if query in INVERTED_INDEX:
-		print "\""+query+"\"" + " is found in: "
-		print INVERTED_INDEX[query][1]
-		
-print '\n\t\t**************** Welcome to me search engine ******************\n\n'
+	query = raw_input("Look for: "+'\033[92m')
+	print '\r\033[0m\x1b[0m'
+	# Keep program running until ':q' is introduced
+	while query != ':q':
+		if query in INVERTED_INDEX:
+			INVERTED_INDEX[query] = OrderedDict(sorted(INVERTED_INDEX[query].items(), key=lambda x: x[1], reverse=True))
+			
+			print style("\n\""+query+"\"" + " is found in: \n","green")
+			getMoreResults = "y"
+			docID = 'someID'
+			docIDStream = iter(INVERTED_INDEX[query])
+			allResultsCount = len(INVERTED_INDEX[query])
+			while (getMoreResults == "y" or getMoreResults == "Y") and docID:
+				displayedResultCount = 0
+				while displayedResultCount < 4 and docID:
+					docID = next(docIDStream, None)
+					if docID:
+						allResultsCount -= 1
+						with open("documents/"+docID+".txt") as file:
+							document = file.read()
+							#get and process the title and body in a document
+							title = titleIndoc(document, query)
+							displayedSnipet = bodyInDoc(document, query)
+							# In case doc pre-processing resulted in words that will give false positive (eg. "B-6P" --> "BP")
+					 		if title[1] is None and displayedSnipet[1] is None:
+					 			continue
+					 		
+							print style('\nArticle '+docID, 'bold')+': '+title[0]+'\n\t'+displayedSnipet[0]+"\n"
+							displayedResultCount += 1
+				if allResultsCount > 0:
+					getMoreResults = raw_input('More results? (y/n) \033[92m')
+					# remove coloring and draw separator
+					print '\r\033[0m\x1b[0m'
+					print '\n===========================================================================================\n'
+		else:
+			print style('No matches found.\n', 'red')
+		query = raw_input("\nLook for: \033[92m")
+	# Clear colors in consol
+	print '\r\033[0m\x1b[0m'	
+			
+print 'loading search engine...'	
+INVERTED_INDEX = json.load(open('inverted_Index.txt','r'))
 
-file = open('inverted_Index.pickle','r')
-INVERTED_INDEX = pickle.load(file)
-
+print '\n\t\t**************** Welcome to Tap Tap Search ******************\n\n'
 
 search()
--- personal/elias/tokenizer.py
+++ personal/elias/tokenizer.py
@@ -1,18 +1,18 @@
-import collections
+import gc
 import glob
-import nltk
+import json
 import os
-import pickle
 import re
-import string
 import sys
+
 from collections import OrderedDict
 from nltk.corpus import stopwords
 from nltk import  word_tokenize
+from string import translate
 
 #Global Constants
-BLOCK_SIZE = 1024000 #1,024,000Bytes size of block in memory. Not same as on disk
-PATH = '../../ReutersCorpus/'
+PATH = sys.argv[1]
+MAX_MEMORY = int(sys.argv[2]) #1,024,000Bytes size of block in memory. Not same as on disk
 
 def getPostings(fileName):
 	with open(PATH + fileName, 'r') as myFile:
@@ -20,7 +20,15 @@ def getPostings(fileName):
 
 	documents = re.split("<REUTERS", data)
 	del documents[0]
-	stopwordsList = set(stopwords.words('english'))
+	
+	# Declaring two lists of stopwords for analysis purposes
+	stopwordsList150 = set(stopwords.words('english'))
+	stopwordsList25 = set(['a','an','and','are','as','at', 'be', 'by', 
+					'for', 'from', 'has', 'he', 'in', 'is', 'it', 
+					'its', 'of', 'on', 'that', 'the', 'to', 'was', 
+					'were', 'will', 'with'
+					])
+
 	postings = []
 	# Only keeps text from TITLE and BODY without tags or HTML symbols.
 	for i in range(len(documents)):
@@ -37,7 +45,7 @@ def getPostings(fileName):
 			title = title[0]
 		else:
 			title = ''
-		
+
 		# Get the body
 		body = re.findall(r'<BODY>(.*?)</BODY>', documents[i])
 		if body:
@@ -45,65 +53,80 @@ def getPostings(fileName):
 		else:
 			body = ''
 
-		documents[i] = title + ' ' + body
-		documents[i] = re.sub(r'&#.+;', ' ', documents[i])
-		documents[i] = re.sub(r'&lt;', '<', documents[i])
-		documents[i] = re.sub(r'&gt;', '>', documents[i])
-		# 1. Remove numbers
-		documents[i] = re.sub(r'\d+', '', documents[i])
-		# 2. Remove non-ASCII values
-		documents[i] = re.sub(r'[^\x00-\x7F]+', ' ', documents[i])
-		# 3. Tokenization of words and removal of punctuation
-		documents[i] = nltk.word_tokenize(documents[i].translate(None, string.punctuation))
-		# 4. Removing Stopwords
-		documents[i] = [word for word in documents[i] if word.lower() not in stopwordsList]
-		for token in documents[i]:
-			postings.append((token, docID))
+		if title != '' or body != '':
+			documents[i] = title + '#####' + body
+			documents[i] = re.sub(r'&#.+;', ' ', documents[i], )
+			documents[i] = re.sub(r'&lt;', '<', documents[i])
+			documents[i] = re.sub(r'&gt;', '>', documents[i])
+			documents[i] = re.sub(r'<.{0,9}>', '', documents[i])
+			# Remove appostrophe
+			documents[i] = re.sub(r'\'s|\'re|\'d|\'ll', '', documents[i])
+			
+			# Remove non-ASCII values
+			documents[i] = re.sub(r'[^\x00-\x7F]+', ' ', documents[i])
+			# Remove unneeded spaces
+			documents[i] = re.sub(r'\s{2,5}', ' ', documents[i])
+
+			output = open("documents/" + docID + ".txt","w")
+			output.write(documents[i])
+			output.close()
+			
+			documents[i] = re.sub(r'#####', '. ', documents[i])
+# 			documents[i] = documents[i].replace('\n',' ')
+			# 1. Remove numbers
+			documents[i] = re.sub(r'\d+', '', documents[i])
+			# 2. Tokenization of words and removal of punctuation
+			documents[i] = nltk.word_tokenize(documents[i].translate(None, string.punctuation))
+			# 3. Removing Stopwords
+			documents[i] = [word.lower() for word in documents[i] if word.lower() not in stopwordsList150]
+			
+			for token in documents[i]:
+				postings.append((token, docID))
+				
 	return postings
 
 def getNewFileName():
-	if not os.path.isfile('temp_inverted_index_1.pickle'):
-		return 'temp_inverted_index_1.pickle'
+	if not os.path.isfile('temp_inverted_index_1.txt'):
+		return 'temp_inverted_index_1.txt'
 	else:
-		return 'temp_inverted_index_'+str(int(re.findall(r'index_(.*?).pickle', max(glob.iglob('*.pickle'), key=os.path.getctime))[0])+1)+'.pickle'
+		return 'temp_inverted_index_'+str(int(re.findall(r'index_(.*?).txt', max(glob.iglob('*.txt'), key=os.path.getctime))[0])+1)+'.txt'
 
 def sortTerms(dictionary):
 	return OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
 
-def writeToDisk(sortedDictionary, fileName):
-	output = open(fileName, 'w')
-	pickle.dump(sortedDictionary, output)
-	output.close()
-	print 'created file '+fileName
+def writeJsonToFile(object, fileName):
+	with open(fileName, 'w') as output:
+		json.dump(object, output)
+		output.close()
+	print 'created file ' + fileName
 	return True
 
 def sortDocIDs(dictionary):
 	for term in dictionary:
-		dictionary[term][1] = OrderedDict(sorted(dictionary[term][1].items(), key=lambda t: int(t[0])))
+		dictionary[term] = OrderedDict(sorted(dictionary[term].items(), key=lambda t: int(t[0])))
 	return dictionary
 
-def spimiInvert(token_stream, blockSize):
+def spimiInvert(token_stream, maxMemory):
 	fileName = getNewFileName()
 	dictionary = {}
-	while (sys.getsizeof(dictionary) <= blockSize):
+	while (sys.getsizeof(dictionary) <= maxMemory):
 		posting = next(token_stream, None)
 		if posting: #[term, docID]
 			if posting[0] not in dictionary:
-				# Stored in format {term: [docFreq, {docID:termFreq, docID:termFrec ...}}
-				dictionary[posting[0]] = [1, {posting[1]:1}]
+				# Stored in format {term: {docID:termFreq, docID:termFrec ...}}
+				dictionary[posting[0]] = {posting[1]:1}
 			else:
-				if posting[1] not in dictionary[posting[0]][1]:
-					dictionary[posting[0]][1][posting[1]] = 1
-					dictionary[posting[0]][0] += 1 # Increment document frequency
+				if posting[1] not in dictionary[posting[0]]:
+					dictionary[posting[0]][posting[1]] = 1
 				else:
-					dictionary[posting[0]][1][posting[1]] += 1
+					dictionary[posting[0]][posting[1]] += 1
 		else:
 			break
 	
 	if len(dictionary) > 0:
 		dictionary = sortTerms(dictionary)
-		dictionary = sortDocIDs(dictionary)
-		return writeToDisk(dictionary, fileName)
+# 		dictionary = sortDocIDs(dictionary)
+		return writeJsonToFile(dictionary, fileName)
 	else:
 		return False
 
@@ -120,23 +143,21 @@ def mergDicts(dict1, dict2):
 				if term1 not in invertedIndex:
 					invertedIndex[term1] = dict1[term1]
 				else:
-					for docID in dict1[term1][1]:
-						if docID in invertedIndex[term1][1]:
-							invertedIndex[term1][1][docID] += dict1[term1][1][docID]
+					for docID in dict1[term1]:
+						if docID in invertedIndex[term1]:
+							invertedIndex[term1][docID] += dict1[term1][docID]
 						else:
-							invertedIndex[term1][1][docID] = dict1[term1][1][docID]
-							invertedIndex[term1][0] += 1
+							invertedIndex[term1][docID] = dict1[term1][docID]
 				term1 = next(iter1,None)
 			elif term2 < term1:
 				if term2 not in invertedIndex:
 					invertedIndex[term2] = dict2[term2]
 				else:
-					for docID in dict2[term2][1]:
-						if docID in invertedIndex[term2][1]:
-							invertedIndex[term2][1][docID] += dict2[term2][1][docID]
+					for docID in dict2[term2]:
+						if docID in invertedIndex[term2]:
+							invertedIndex[term2][docID] += dict2[term2][docID]
 						else:
-							invertedIndex[term2][1][docID] = dict2[term2][1][docID]
-							invertedIndex[term2][0] += 1
+							invertedIndex[term2][docID] = dict2[term2][docID]
 				term2 = next(iter2,None)
 		
 		if term1 is None:
@@ -144,49 +165,48 @@ def mergDicts(dict1, dict2):
 				if term2 not in invertedIndex:
 					invertedIndex[term2] = dict2[term2]
 				else:
-					for docID in dict2[term2][1]:
-						if docID in invertedIndex[term2][1]:
-							invertedIndex[term2][1][docID] += dict2[term2][1][docID]
+					for docID in dict2[term2]:
+						if docID in invertedIndex[term2]:
+							invertedIndex[term2][docID] += dict2[term2][docID]
 						else:
-							invertedIndex[term2][1][docID] = dict2[term2][1][docID]
-							invertedIndex[term2][0] += 1
+							invertedIndex[term2][docID] = dict2[term2][docID]
 				term2 = next(iter2,None)
 		else:
 			while term1 != None:
 				if term1 not in invertedIndex:
 					invertedIndex[term1] = dict1[term1]
 				else:
-					for docID in dict1[term1][1]:
-						if docID in invertedIndex[term1][1]:
-							invertedIndex[term1][1][docID] += dict1[term1][1][docID]
+					for docID in dict1[term1]:
+						if docID in invertedIndex[term1]:
+							invertedIndex[term1][docID] += dict1[term1][docID]
 						else:
-							invertedIndex[term1][1][docID] = dict1[term1][1][docID]
-							invertedIndex[term1][0] += 1
+							invertedIndex[term1][docID] = dict1[term1][docID]
 				term1 = next(iter1,None)
 	return invertedIndex
 
 
-def mergeFiles(pickleFiles):
+def mergeFiles(jsonFiles):
 	invertedIndex = OrderedDict()	
-	fileName = next(pickleFiles, None)
+	fileName = next(jsonFiles, None)
 	while fileName != None:
 		print '\nMerging file: ' + fileName
-		dict = pickle.load(open(fileName,'r'))
+		dict = json.load(open(fileName,'r'))
 		invertedIndex = mergDicts(dict, invertedIndex)
 		# get the next file to merge
-		fileName = next(pickleFiles, None)
-	writeToDisk(invertedIndex, "inverted_Index.pickle")
+		fileName = next(jsonFiles, None)
+# 	invertedIndex = sortDocIDs(invertedIndex)
+	writeJsonToFile(invertedIndex, "inverted_Index.txt")
 
 # START INDEXING PROGRAM
 print '\nProcessing documents...'
 postings = []
-for fileName in os.listdir('../../ReutersCorpus'):#sys.argv[1]):
+for fileName in os.listdir(PATH):
 	if fileName.endswith('.sgm'):
 		postings += getPostings(fileName)
 
 token_stream = iter(postings)
-while spimiInvert(token_stream, BLOCK_SIZE):
+while spimiInvert(token_stream, MAX_MEMORY):
 	pass
 
 # Start Merging
-mergeFiles(iter(glob.iglob('*.pickle')))
+mergeFiles(iter(glob.iglob('*.txt')))
