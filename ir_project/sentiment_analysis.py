8bf68a06287bd430761f2a2748c02ab7df94f9c6
Elias Haroun <elias.haroun@gmail.com>
2016-12-01 03:33:20 -0500

    Added tokenizer and sentiment analysis

---
+++ ir_project/sentiment_analysis.py
@@ -0,0 +1,137 @@
+import codecs
+import sys
+import json
+import os
+from afinn import Afinn
+from nltk import sent_tokenize
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+from vaderSentiment.vaderSentiment import sentiment as vaderSentiment
+
+# Path to corpus
+PATH = sys.argv[1]
+
+# Afinn Sentiment Analysis
+afinn = Afinn()
+
+# Google Sentiment Analysis
+credentials = GoogleCredentials.get_application_default()
+service = discovery.build('language', 'v1', credentials=credentials)
+
+# Google Sentiment Analysis API https://cloud.google.com/natural-language/docs/basics
+"""score of the sentiment ranges between -1.0 (negative) and 1.0 (positive) and corresponds
+to the overall emotional leaning of the text.magnitude indicates the overall strength of emotion 
+(both positive and negative) within the given text, between 0.0 and +inf. 
+Unlike score, magnitude is not normalized; each expression of emotion within the text 
+(both positive and negative) contributes to the text's magnitude (so longer text blocks may have greater magnitudes)."""
+def documentGoogleSA(document):
+	service_request = service.documents().analyzeSentiment(
+	  body={
+	    'document': {
+	      'type': 'PLAIN_TEXT',
+	      "language": "EN",
+	      'content': document,
+	    },
+	    "encodingType":"UTF8",
+	  }
+	)
+
+	result = service_request.execute()['documentSentiment']
+	return  {'score': result['score'] * 5.0, 'magnitude': result['magnitude']} # normalized resutls, between -5 and +5
+
+# Document Vader Sentiment Analysis https://github.com/cjhutto/vaderSentiment
+# https://www.linkedin.com/pulse/sentiment-analysis-using-vader-muthuraj-kumaresan
+"""Score ranges between -4.0 and 4.0
+The output provides the polarity and intensity of the inputs. 
+The compound in the output provides the sentiment intensity and will be
+in the range of -1 (Extremely negative) to 1 (Extremely positive).
+Based on the compound value, you can create more ranges to define more categorical 
+values for sentiment(Extremely positive, positive, neutral, negative, Extremely negative)."""
+def documentVaderSA(document):
+	result = {'pos':[], 'neg':[], 'compound':[], 'neu':[]}
+
+	sentences = sent_tokenize(document)
+
+	for sentence in sentences:
+		vs = vaderSentiment(sentence)
+		result['pos'].append(vs['pos'])
+    	result['neg'].append(vs['neg'])
+    	result['compound'].append(vs['compound'])
+    	result['neu'].append(vs['neu'])
+
+	for category in result:
+		result[category] = (float(sum(result[category]))/len(result[category])) * 5.0/4.0 # normalized resutls, between -5 and +5
+		result[category] = float("{0:.2f}".format(result[category]))
+
+	return result
+
+# Afinn Sentiment Analysis http://www2.imm.dtu.dk/pubdb/views/edoc_download.php/6006/pdf/imm6006.pdf
+# Score ranges between -5 and +5
+def documentAfinnSA(document):
+	score = 0
+	sentences = sent_tokenize(document)
+
+	for sentence in sentences:
+		score += afinn.score(sentence)
+
+	return float("{0:.2f}".format(float(score)/len(sentences)))
+
+def deptScoreAvg(department_docs):
+	vader_result = {'pos':[], 'neg':[], 'compound':[], 'neu':[]}
+	google_result = {'score': [], 'magnitude': []}
+	afinn_result = 0
+
+	for docId in department_docs:
+		afinn_result += department_docs[docId]['afinn']
+
+		google_result['score'].append(department_docs[docId]['google']['score'])
+		google_result['magnitude'].append(department_docs[docId]['google']['magnitude'])
+
+		vader_result['pos'].append(department_docs[docId]['vader']['pos'])
+		vader_result['neg'].append(department_docs[docId]['vader']['neg'])
+		vader_result['compound'].append(department_docs[docId]['vader']['compound'])
+		vader_result['neu'].append(department_docs[docId]['vader']['neu'])
+
+	afinn_result = float("{0:.2f}".format(float(afinn_result)/len(department_docs)))
+
+	for category in google_result: 
+		google_result[category] = float(sum(google_result[category]))/len(google_result[category])
+		google_result[category] = float("{0:.2f}".format(google_result[category]))
+
+	for category in vader_result:
+		vader_result[category] = float(sum(vader_result[category]))/len(vader_result[category])
+		vader_result[category] = float("{0:.2f}".format(vader_result[category]))
+
+	return {'google': google_result, 'vader': vader_result, 'afinn': afinn_result}
+
+def writeJsonToFile(object, fileName):
+	with open(fileName, 'w') as output:
+		json.dump(object, output)
+		output.close()
+	print 'created file ' + fileName
+	return True
+
+if __name__ == '__main__':
+
+	print 'Starting Sentiment Analysis...'
+	docId = 1
+	analysis_summary = {}
+	for fileName in os.listdir(PATH):
+		if fileName.endswith('.json'):
+			file = open(PATH + '/' + fileName)
+			documents = json.load(file)
+			department = {}
+			for document in documents:
+				document_str = ''
+				department[docId] = {}
+				document_str = '. '.join([document_str, document['title'], document['body']]).encode('ascii', 'ignore')
+
+				# Save normalized resutls, between -5 and +5
+				department[docId]['google'] = documentGoogleSA(document_str)
+				department[docId]['vader'] = documentVaderSA(document_str)
+				department[docId]['afinn'] = documentAfinnSA(document_str)
+				docId += 1
+			writeJsonToFile(department, 'sentiment/' + fileName)
+			analysis_summary[fileName.replace('.json','')] = deptScoreAvg(department)
+
+	writeJsonToFile(analysis_summary, 'sentiment/' + 'summary.json')
--- ir_project/tokenizer.py
+++ ir_project/tokenizer.py
@@ -8,6 +8,7 @@ import string
 import sys
 
 from collections import OrderedDict
+from math import log10
 from nltk.corpus import stopwords
 from nltk import  word_tokenize
 from nltk.stem.porter import PorterStemmer
@@ -18,10 +19,11 @@ PATH = sys.argv[1]
 MAX_MEMORY = int(sys.argv[2]) #1,024,000Bytes size of block in memory. Not same as on disk
 
 docLengthDict = {} 	# Store a dictionary of documents and their length
+docIdDict = {} 	# Store a dictionary of documents and their length
 docID = 0
 
 def getPostings(fileName):
-	global docID, docLengthDict
+	global docID, docLengthDict, docIDDict
 
 	with open(PATH + fileName, 'r') as myFile:
 		documents = json.load(myFile)
@@ -40,47 +42,52 @@ def getPostings(fileName):
 		# Get the document
 		document = ' '.join([documents[i]['title'], documents[i]['body']])
 
-		if len(document) > 0:
-			# Get the document ID
-			docID += 1
-	
-			# Remove non-ASCII characters
-			re.sub(r'[^\x00-\x7F]+', ' ', document)
-						
-			# Tokenization of words and removal of punctuation
-			# table = string.maketrans(string.punctuation, "                                ")
-			# document = nltk.word_tokenize(document.translate(table))
-			document = nltk.word_tokenize(document.translate(None, string.punctuation))
+		# Get the document ID
+		docID += 1
 
-			docLengthDict[docID] = len(document)
-			# ***************************    Lossy Compression    *******************************
-			#====================================================================================
-			
-			# Remove apostrophe
-			document = [re.sub(r'\b\'m|\'s|\'re|\'d|\'ll|n\'t\b', '', token) for token in document]
- 			# No numbers
-			document = [re.sub(r'\b\d+\.?\d*\b', '', token) for token in document]
-			document = filter(lambda a: a != '', document)
- 			# Case folding
+		# Ensure characters encoding
+		document = document.encode('UTF-8', 'ignore')
+
+		# Remove URLs from document
+		url_pattern1 = r'(https?|ftp):\/\/\/?[^\s\/$.?#].[^\s]*'
+		url_pattern2 = r'www\.[^\s\/$.?#].[^\s]*'	
+		document = re.sub(url_pattern1, ' ', document)
+		document = re.sub(url_pattern2, ' ', document)
+
+		# Tokenization of words and removal of punctuation
+		document = nltk.word_tokenize(document.translate(None, string.punctuation))
+
+		docLengthDict[docID] = len(document)
+		docIdDict[docID] = documents[i]['url']
+		# ***************************    Lossy Compression    *******************************
+		#====================================================================================
+		
+		# Remove apostrophe
+		document = [re.sub(r'\b\'m|\'s|\'re|\'d|\'ll|n\'t\b', '', token) for token in document]
+		# No numbers
+		document = [re.sub(r'\b(\d+\w+)|(\w+\d+)|(\d+\.?\d*\w*)\b', '', token) for token in document]
+		# Remove empty strings
+		document = filter(lambda a: a != '', document)
+			# Case folding
 # 			documents[i] = [token.lower() for token in documents[i]]
- 			# Removing Stopwords 25
+			# Removing Stopwords 25
 # 			documents[i] = [token for token in documents[i] if token not in stopwordsList25]
- 			# Removing Stopwords 150
-			document = [token for token in document if token not in stopwordsList150]
- 			# Stemming (Porter)
+			# Removing Stopwords 150
+		document = [token for token in document if token not in stopwordsList150]
+			# Stemming (Porter)
 # 			stemmer = PorterStemmer()
 # 			documents[i] = [stemmer.stem(token) for token in documents[i]]
-			
-			for token in document:
-				postings.append((token, docID))
+		
+		for token in document:
+			postings.append((token, docID))
 				
 	return postings
 
 def getNewFileName():
-	if not os.path.isfile('temp_inverted_index_1.txt'):
-		return 'temp_inverted_index_1.txt'
+	if not os.path.isfile('temp_inverted_index_1.json'):
+		return 'temp_inverted_index_1.json'
 	else:
-		return 'temp_inverted_index_'+str(int(re.findall(r'index_(.*?).txt', max(glob.iglob('*.txt'), key=os.path.getctime))[0])+1)+'.txt'
+		return 'temp_inverted_index_'+str(int(re.findall(r'index_(.*?).json', max(glob.iglob('*.json'), key=os.path.getctime))[0])+1)+'.json'
 
 def sortDict(dictionary):
 	return OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
@@ -183,8 +190,24 @@ def mergeFiles(jsonFiles):
 		dict = json.load(open(fileName,'r'), object_pairs_hook = OrderedDict)
 		invertedIndex = mergDicts(dict, invertedIndex)
 		# get the next file to merge
+		os.remove(fileName)
 		fileName = next(jsonFiles, None)
-	writeJsonToFile(invertedIndex, "inverted_index.txt")
+	writeJsonToFile(invertedIndex, "indexes/inverted_index.json")
+
+def index_tf_idf(path):
+	with open(path + "inverted_index.json", 'r') as inverted_index_file:
+		inverted_index = json.load(inverted_index_file)
+
+	with open(path + "docs_lengths.json", 'r') as docs_lengths_file:
+		N = len(json.load(docs_lengths_file))
+
+	for term in inverted_index:
+		for docId in inverted_index[term]:
+			tf = inverted_index[term][docId]
+			df = len(inverted_index[term])
+			inverted_index[term][docId] = tf * log10(N/float(df))
+
+	writeJsonToFile(inverted_index, path + "tf-idf_index.json")
 
 # START INDEXING PROGRAM
 print '\nProcessing documents...'
@@ -194,11 +217,15 @@ for fileName in os.listdir(PATH):
 		postings += getPostings(fileName)
 
 # Store a dictionary of documents and their length
-writeJsonToFile(docLengthDict, "indexes/docs_lengths.txt")
+writeJsonToFile(docLengthDict, "indexes/docs_lengths.json")
+writeJsonToFile(docIdDict, "indexes/docIdDict.json")
 
 token_stream = iter(postings)
 while spimiInvert(token_stream, MAX_MEMORY):
 	pass
 
 # Start Merging
-mergeFiles(iter(glob.iglob('*.txt')))
+mergeFiles(iter(glob.iglob('*.json')))
+
+# Generate a tf-idf index
+index_tf_idf("indexes/")
